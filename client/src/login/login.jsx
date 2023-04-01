38c5a2832f38cf25a7414f1918faaef06c3fe639
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-04-02 00:08:21 +0530

    login

--- client/src/login/login.jsx
+++ client/src/login/login.jsx
@@ -1,7 +1,22 @@
 import { Button, ButtonGroup, FormControl, FormErrorMessage, FormLabel, Heading, Input, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 function Login() {
+	const formik = useFormik({
+		initialValues: {
+			username: '',
+			password: '',
+		},
+		validationSchema: Yup.object({
+			username: Yup.string().min(3, 'Must be 3 characters or more').max(15, 'Must be 15 characters or less').required('Required'),
+			password: Yup.string().min(8, 'Must be 8 characters or more').max(20, 'Must be 20 characters or less').required('Required'),
+		}),
+		onSubmit: (values) => {
+			console.log(values)
+		},
+	})
 	return (
 		<VStack as='form' w={{ base: '90%', md: '500px' }} m='auto' justify='center' h='100vh' spacing='2em'>
 			<Heading>Log In</Heading>
