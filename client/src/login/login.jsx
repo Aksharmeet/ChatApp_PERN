ec3dfd95639a7dae92ea8360a0b7578e5cfc8ce6
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-04-02 00:39:06 +0530

    login

--- client/src/login/login.jsx
+++ client/src/login/login.jsx
@@ -1,42 +1,40 @@
-import { Button, ButtonGroup, FormControl, FormErrorMessage, FormLabel, Heading, Input, VStack } from '@chakra-ui/react'
+import { Button, ButtonGroup, Heading, VStack } from '@chakra-ui/react'
 import React from 'react'
-import { useFormik } from 'formik'
+import { Form, Formik } from 'formik'
 import * as Yup from 'yup'
+import TextField from '../components/textField'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
-	const formik = useFormik({
-		initialValues: {
-			username: '',
-			password: '',
-		},
-		validationSchema: Yup.object({
-			username: Yup.string().min(3, 'Must be 3 characters or more').max(15, 'Must be 15 characters or less').required('Required'),
-			password: Yup.string().min(8, 'Must be 8 characters or more').max(20, 'Must be 20 characters or less').required('Required'),
-		}),
-		onSubmit: (values) => {
-			console.log(values)
-		},
-	})
+	const navigate = useNavigate()
+
 	return (
-		<VStack as='form' w={{ base: '90%', md: '500px' }} m='auto' justify='center' h='100vh' spacing='2em'>
-			<Heading>Log In</Heading>
-			<FormControl>
-				<FormLabel fontSize='lg'>Username</FormLabel>
-				<Input name='username' placeholder='Enter Username' autoComplete='off' size='lg' />
-				<FormErrorMessage>Invalid Username</FormErrorMessage>
-			</FormControl>
-			<FormControl>
-				<FormLabel fontSize='lg'>Password</FormLabel>
-				<Input name='password' placeholder='Enter Password' autoComplete='off' size='lg' />
-				<FormErrorMessage>Invalid Password</FormErrorMessage>
-			</FormControl>
-			<ButtonGroup>
-				<Button colorScheme='teal' type='submit'>
-					Login
-				</Button>
-				<Button>Create Account</Button>
-			</ButtonGroup>
-		</VStack>
+		<Formik
+			initialValues={{
+				username: '',
+				password: '',
+			}}
+			validationSchema={Yup.object({
+				username: Yup.string().min(3, 'Must be 3 characters or more').max(15, 'Must be 15 characters or less').required('Required'),
+				password: Yup.string().min(8, 'Must be 8 characters or more').max(20, 'Must be 20 characters or less').required('Required'),
+			})}
+			onSubmit={(values, actions) => {
+				alert(JSON.stringify(values, null, 2))
+				actions.resetForm()
+			}}
+		>
+			<VStack as={Form} w={{ base: '90%', md: '500px' }} m='auto' justify='center' h='100vh' spacing='1em'>
+				<Heading>Log In</Heading>
+				<TextField name='username' placeholder='Enter Username' autcomplete='off' label='Username' />
+				<TextField name='password' placeholder='Enter Password' autcomplete='off' label='Password' />
+				<ButtonGroup>
+					<Button onClick={() => navigate('/register')}>Create Account</Button>
+					<Button colorScheme='teal' type='submit'>
+						Login
+					</Button>
+				</ButtonGroup>
+			</VStack>
+		</Formik>
 	)
 }
 
--- client/src/login/signup.jsx
+++ client/src/login/signup.jsx
@@ -1,7 +1,44 @@
+import { Button, ButtonGroup, Heading, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { Form, Formik } from 'formik'
+import * as Yup from 'yup'
+import TextField from '../components/textField'
+import { useNavigate } from 'react-router-dom'
+import { ArrowBackIcon } from '@chakra-ui/icons'
 
 function Signup() {
-	return <div>Signup</div>
+	const navigate = useNavigate()
+
+	return (
+		<Formik
+			initialValues={{
+				username: '',
+				password: '',
+			}}
+			validationSchema={Yup.object({
+				username: Yup.string().min(3, 'Must be 3 characters or more').max(15, 'Must be 15 characters or less').required('Required'),
+				password: Yup.string().min(8, 'Must be 8 characters or more').max(20, 'Must be 20 characters or less').required('Required'),
+			})}
+			onSubmit={(values, actions) => {
+				alert(JSON.stringify(values, null, 2))
+				actions.resetForm()
+			}}
+		>
+			<VStack as={Form} w={{ base: '90%', md: '500px' }} m='auto' justify='center' h='100vh' spacing='1em'>
+				<Heading>Sign Up</Heading>
+				<TextField name='username' placeholder='Enter Username' autcomplete='off' label='Username' />
+				<TextField name='password' placeholder='Enter Password' autcomplete='off' label='Password' />
+				<ButtonGroup>
+					<Button onClick={() => navigate('/')} leftIcon={<ArrowBackIcon />}>
+						Back
+					</Button>
+					<Button colorScheme='teal' type='submit'>
+						Create Account
+					</Button>
+				</ButtonGroup>
+			</VStack>
+		</Formik>
+	)
 }
 
 export default Signup
